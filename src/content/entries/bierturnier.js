d3fb4ca50088ad61948a4cf2e89c8698506e9fb0
Christian Thimm <christianthimm@yahoo.de>
2026-04-28 18:28:57 +0200

    more reference changes

--- src/content/entries/bierturnier.js
+++ src/content/entries/bierturnier.js
@@ -10,14 +10,14 @@ export default {
   name: { en: 'Bierturnier', de: 'Bierturnier' },
   content: {
     en: `# Bierturnier 
-![](/images/bierturnier_lightmode.png|public/images/bierturnier.png)
+![](/images/bierturnier_lightmode.png|/images/bierturnier.png)
 An [App](https://tournament.beer/de) developed with Friends to organize Beer Pong Tournaments with friends.
 ![](/images/bierturnier1.png){natural}
 ![](/images/bierturnier2.png){natural}
 ![](/images/bierturnier3.png){natural}
 `,
     de: `
-![](/images/bierturnier_lightmode.png|public/images/bierturnier.png)
+![](/images/bierturnier_lightmode.png|/images/bierturnier.png)
 
 Eine [App](https://tournament.beer/de) entwickelt mit Freunden um mit Freunden Bierpongturniere zu organisieren.
 ![](/images/bierturnier1.png){natural}
